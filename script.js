37c876de39000e799557ea9e526f0fa5d52ea01c
iluvredwall <bobomb.asplode@gmail.com>
2014-04-22 22:35:59 -0700

    cleaned up code, re-fixed day view, minor improvements to day view  - clock changes (still needs options)   - moved updateClock to run when rest of schedule is updated   - removed seconds   - moved to top right of page   - changed color from green to gray   - cleaned up updateClock code  - removed update button (will probably run updateSchedule() when clock is clicked)  - made day view only appear on mobile (needs options)   - fixed "future week" warning appearning on the current week in day view   - updated warning message to reflect the fact that the user might not be viewing an entire week

--- script.js
+++ script.js
@@ -9,20 +9,23 @@
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; //days of the week in string form
 var urlParams; //object with GET variables as properties and their respective values as values
 var schedules; //array of schedules (each schedule is an array in this array
-var dispWeek; //Sunday of week currently being displayed by the schedule
+var displayDate; //Sunday of week currently being displayed by the schedule
 var mobile = isMobile();
 var updateScheduleID; //ID of interval of updateSchedule
 var options = new Object();
-var ClockUpdateRate = setInterval(function(){startTime()},500);
 var hasFocus = true; //document.hasFocus() seems to be unreliable; assumes window has focus on page load
 
 document.addEventListener("visibilitychange", function(event) {
-	if(!document.hidden) updateSchedule(); //only slightly redundant; on un-minimize, document gains visibility without focus
+	if(!document.hidden) { //only slightly redundant; on un-minimize, document gains visibility without focus
+		updateSchedule();
+		updateClock();
+	}
 	updateUpdateInterval();
 });
 
 addEventListener("focus", function(event) {
 	updateSchedule();
+	updateClock();
 	
 	hasFocus = true;
 	updateUpdateInterval();
@@ -51,9 +54,10 @@ addEventListener("load", function(event) {
 	
 	parseRawSchedule();
 
-	setDispWeek();
+	setDisplayDate();
 	setHighlightedPeriod();
 	
+	updateClock();
 });
 
 function initTitle() {
@@ -106,7 +110,7 @@ function parseRawSchedule() {
 /**
  * Displays schedule of the week of the given date/time
  */
-function setDispWeek(time, force) {
+function setDisplayDate(time, force) {
 	if(!time)
 	{
 		time = new Date(); //set default time to now
@@ -119,15 +123,14 @@ function setDispWeek(time, force) {
 	}
 	
 	var date = new Date(time); //variable to keep track of current day in loop
-	if(false)
-	getSunday(date);
+	if(!mobile) getSunday(date);
 	
-	if(force || !dispWeek || (date.valueOf()!=dispWeek.valueOf())){
+	if(force || !displayDate || (date.valueOf()!=displayDate.valueOf())){
 		var schedule = document.getElementById("schedule"); //get schedule table
 		
-		dispWeek = new Date(date);
+		displayDate = new Date(date);
 		
-		if(date>getSunday(new Date()))
+		if(date > (mobile ? new Date() : getSunday(new Date())))
 			document.getElementById("warning").style.display = "block"; //display warning if week is in the future
 		else document.getElementById("warning").style.display = "none"; //else hide warning
 		
@@ -139,7 +142,7 @@ function setDispWeek(time, force) {
 		
 		var week = schedule.insertRow(-1); //create new week (row)
 		
-		if(false)
+		if(!mobile)
 			for(var d=0;d<5;d++) { 
 				//for each day Monday through Friday (inclusive)
 				date.setDate(date.getDate()+1); //increment day
@@ -365,7 +368,7 @@ function createSubPeriods(parent, name, start1, end1, start2, end2, date) {
  * Navigates schedule to previous week.
  */
 function goLastWeek() {
-	var week = new Date(dispWeek); //change schedule
+	var week = new Date(displayDate); //change schedule
 	week.setDate(week.getDate() - 7);
 	updateSchedule(week);
 	
@@ -383,7 +386,7 @@ function goLastWeek() {
  * Navigates schedule to next week.
  */
 function goNextWeek() {
-	var week = new Date(dispWeek); //change schedule
+	var week = new Date(displayDate); //change schedule
 	week.setDate(week.getDate() + 7);
 	updateSchedule(week);
 	
@@ -480,10 +483,11 @@ function setHighlightedPeriod(time) {
 }
 
 /**
- * Updates schedule to display as it would on the given date/time; defaults to now if none is given
+ * Updates schedule to display as it would on the given date/time; defaults to now if none is given.
+ * Also updates
  */
 function updateSchedule(time,force) {
-	setDispWeek(time,force);
+	setDisplayDate(time,force);
 	setHighlightedPeriod();
 }
 /**
@@ -624,7 +628,10 @@ function updateUpdateInterval() {
 function setUpdateInterval(seconds) {
 	clearInterval(updateScheduleID);
 	if(seconds>0)
-		updateScheduleID = setInterval("updateSchedule()", seconds * 1000); //Convert to milliseconds.
+		updateScheduleID = setInterval(function() {
+			updateClock();
+			updateSchedule();
+		}, seconds * 1000); //convert to milliseconds
 	else updateScheduleID = null;
 }
 
@@ -637,25 +644,21 @@ function isMobile() {
 	if(window.innerWidth <= 800 && window.innerHeight <= 600) return true;
 	return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4));
 }
-//Shamelessly stolen from certain website
-function startTime()
-{
-var today=new Date();
-var h=today.getHours();
-var m=today.getMinutes();
-var s=today.getSeconds();
-m=checkTime(m);
-s=checkTime(s);
-NH=h%12 //modding out by twelve to fix military time issue
-document.getElementById('CurrentTime').innerHTML=NH+":"+m+":"+s;
-//setInterval(function(){startTime()},500);
+
+/**
+ * Updates the time on the bell schedule
+ */
+function updateClock() {
+	var now = new Date();
+	var h = now.getHours();
+	var m = now.getMinutes();
+	document.getElementById('currentTime').innerHTML = (h%12) + ":" + addLeadingZero(m) + (h>12 ? " PM" : " AM");
 }
 
-function checkTime(i) //Used to fix formatting, adds zeros before single digit times
-{
-if (i<10)
-  {
-  i="0" + i;
-  }
-return i;
+/**
+ * Adds leading zeroes as necessary to make output (at least) 2 characters long
+ * (Assumes that n is an integer.)
+ */
+function addLeadingZero(n) {
+	return (n<10) ? "0"+n : n;
 }
