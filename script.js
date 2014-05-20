486424ee1be25eb6333adf86dc48dfdbe69038ed
iluvredwall <bobomb.asplode@gmail.com>
2014-05-19 21:51:26 -0700

    removed AP good luck notification

--- script.js
+++ script.js
@@ -119,12 +119,10 @@ function setDispWeek(time,force){
 		
 		if(date > getSunday(new Date())) {
 			document.getElementById("warning").style.display = "block"; //display warning if week is in the future
-			document.getElementById("news").style.display = "none";
 		}
 		else 
-		{	
+		{
 			document.getElementById("warning").style.display = "none"; //else hide warning
-			document.getElementById("news").style.display = "block";
 		}
 		
 		/*
