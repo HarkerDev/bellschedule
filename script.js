370e1cb20df91cdfb1ad499dfbc07a6b2b1afc84
iluvredwall <bobomb.asplode@gmail.com>
2014-05-31 11:51:53 -0700

    added finals schedules and summer break; added finals good luck message

--- script.js
+++ script.js
@@ -180,7 +180,7 @@ function setDisplayDate(time, force) {
 
 		if(date > (mobile ? getDayBegnning(new Date()) : getSunday(new Date())))
 			warn("This is a future week, so the schedule may be incorrect. (In particular, special/alternate schedules may be missing.)"); //display warning if week is in the future
-		else warn("Good luck on your AP Exams!"); //else display good luck message
+		else warn("Good luck on your finals!"); //else display good luck message
 
 		/*
 		if(date.valueOf()==getSunday(new Date()).valueOf()) document.getElementById("currWeek").style.display = "none"; //hide back to current week button on current week
