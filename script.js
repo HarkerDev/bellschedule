9694447988b1538c803d177d52f7ea75e03bfbfe
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2017-08-24 22:32:43 -0700

    Update link

--- script.js
+++ script.js
@@ -212,7 +212,7 @@ function setDisplayDate(time, force) {
 
 	displayDate = new Date(date);
 
-	displayMessage = "Welcome back! Tell a freshman about <a href='http://tiny.cc/bellschedule'>tiny.cc/bellschedule</a>.";
+	displayMessage = "Welcome back! Tell a freshman about <a href='http://tiny.cc/hsbell'>tiny.cc/hsbell</a>.";
 	if(isSameDate(date, new Date("12/12/16")))
 		displayMessage += "<span style='font-weight:bold;color: red;'>Finals Disclaimer:</span> All morning exams have different call times divided by grade or course that are not displayed here. To see when you need to get to school, check out the official schedule <a href='http://resources.harker.org/download/us-final-exams-schedule/'>here</a>."
 	if(getMonday(date) > getMonday(new Date()))
