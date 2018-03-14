785382dd04434f3f1b2cfc984eb81ad141c7c109
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2018-03-13 19:09:17 -0700

    Update message

--- scripts/script.js
+++ scripts/script.js
@@ -221,8 +221,9 @@ function setDisplayDate(time, force) {
 
         displayDate = new Date(date);
 
-        displayMessage = "Peep HarkerDev and our work @ <a href='https://harkerdev.github.io/'>tiny.cc/harkerdev</a>."
-        displayMessage += "<br>Hungry? <a href='https://n3a9.github.io/harker-lunch/'>tiny.cc/lunchmenu</a>.";
+        //displayMessage = "Peep HarkerDev and our work @ <a href='https://harkerdev.github.io/'>tiny.cc/harkerdev</a>."
+        //displayMessage += "<br>Hungry? <a href='https://n3a9.github.io/harker-lunch/'>tiny.cc/lunchmenu</a>.";
+        displayMessage = "Donate to the victims of the MSD shooting <a href='https://www.gofundme.com/stonemandouglasvictimsfund'>here</a>."
         if (getMonday(date) > getMonday(new Date())) {
             displayMessage += "<br>Future schedules may be incorrect."; //display warning if date is in the future
         }
